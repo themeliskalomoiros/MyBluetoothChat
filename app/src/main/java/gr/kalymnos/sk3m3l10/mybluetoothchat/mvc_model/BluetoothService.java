157e6d6ec08d4aa1a0cb340c86785327c9fdcfc7
Tomas Michael <tomasmichael995@gmail.com>
2018-11-06 01:57:20 +0200

    Created BluetoothService abstract class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_model/BluetoothService.java
@@ -0,0 +1,43 @@
+package gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_model;
+
+import android.bluetooth.BluetoothAdapter;
+
+import java.util.UUID;
+import java.util.logging.Handler;
+
+public abstract class BluetoothService {
+
+    protected static final String TAG = "BluetoothService";
+
+    private static final UUID INSECURE_UUID = UUID.fromString("8b10b125-076a41a2b1ca-7c37b56bc3a7");
+
+    public static final String ACTION_REQUEST_ENABLE = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+    public static final int REQUEST_CODE_ENABLE_BT = 155;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private Handler handler;
+
+    public BluetoothService(Handler handler) {
+        this.handler = handler;
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public UUID getUuid() {
+        return INSECURE_UUID;
+    }
+
+    public boolean isBluetoothSupported() {
+        if (bluetoothAdapter != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBluetoothEnabled() {
+        if (bluetoothAdapter.isEnabled()) {
+            return true;
+        }
+        return false;
+    }
+
+}
