ce12e2d65c983f2bb10b2b959e466553441aac3a
Tomas Michael <tomasmichael995@gmail.com>
2018-11-05 20:47:18 +0200

    Initial commit

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_controllers/MainActivity.java
@@ -0,0 +1,15 @@
+package gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gr.kalymnos.sk3m3l10.mybluetoothchat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
