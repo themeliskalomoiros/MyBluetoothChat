108c1b3a6a7057b047febc7881e614555c350dad
Tomas Michael <tomasmichael995@gmail.com>
2018-11-06 02:14:18 +0200

    Created FakeBluetoothServiceImpl class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_model/FakeBluetoothServiceImpl.java
@@ -0,0 +1,9 @@
+package gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_model;
+
+import java.util.logging.Handler;
+
+public class FakeBluetoothServiceImpl extends BluetoothService {
+    public FakeBluetoothServiceImpl(Handler handler) {
+        super(handler);
+    }
+}
