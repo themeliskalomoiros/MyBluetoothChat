45656e0c00ac3f31c85b35b204ea896134d35992
Tomas Michael <tomasmichael995@gmail.com>
2018-11-06 21:21:38 +0200

    Created inner class MainActivityHandler for MainActivity, to override handleMessages()

--- app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_controllers/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_controllers/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -16,6 +17,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements MainScreenViewMvc
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupUi();
-        bluetoothService = new BluetoothServiceImpl(new Handler());
+        bluetoothService = new BluetoothServiceImpl(new MainActivityHandler());
         setupBluetoothRadio();
         getAndDisplayPairedDevices();
         registerDiscoverDevicesReceiver();
@@ -166,7 +168,6 @@ public class MainActivity extends AppCompatActivity implements MainScreenViewMvc
     @Override
     public void onDeviceItemClicked(int position) {
         bluetoothService.startClientMode(devices.get(position));
-
     }
 
     private void setupBluetoothRadio() {
@@ -210,5 +211,16 @@ public class MainActivity extends AppCompatActivity implements MainScreenViewMvc
         viewMvc = new MainScreenViewMvcImpl(LayoutInflater.from(this), null);
         viewMvc.setOnBluetoothScanClickListener(this);
         viewMvc.setOnDeviceItemClickListener(this);
+    }
+
+    private class MainActivityHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case HandlerConstants.CONNECTION_SUCCESS:
+                    Toast.makeText(MainActivity.this, "CONNECTION SUCCESS!", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
     }
 }
