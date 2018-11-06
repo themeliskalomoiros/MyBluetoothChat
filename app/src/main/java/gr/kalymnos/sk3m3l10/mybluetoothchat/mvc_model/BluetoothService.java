7d3e53a715e7c3c95fb2d16f56aa4bb79e9d025d
Tomas Michael <tomasmichael995@gmail.com>
2018-11-06 02:31:22 +0200

    All the action constants for finding a device are provided by the BluetoothService

--- app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_model/BluetoothService.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_model/BluetoothService.java
@@ -2,10 +2,10 @@ package gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_model;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.os.Handler;
 
 import java.util.Set;
 import java.util.UUID;
-import android.os.Handler;
 
 public abstract class BluetoothService {
 
@@ -14,6 +14,10 @@ public abstract class BluetoothService {
     private static final UUID INSECURE_UUID = UUID.fromString("8b10b125-076a41a2b1ca-7c37b56bc3a7");
 
     public static final String ACTION_REQUEST_ENABLE = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+    public static final String ACTION_DISCOVERY_STARTED = BluetoothAdapter.ACTION_DISCOVERY_STARTED;
+    public static final String ACTION_DISCOVERY_FINISHED = BluetoothAdapter.ACTION_DISCOVERY_FINISHED;
+    public static final String ACTION_DEVICE_FOUND = BluetoothDevice.ACTION_FOUND;
+    public static final String EXTRA_DEVICE = BluetoothDevice.EXTRA_DEVICE;
     public static final int REQUEST_CODE_ENABLE_BT = 155;
 
     private BluetoothAdapter bluetoothAdapter;
