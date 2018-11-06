314ebb9a45beb7d373a9dc72abea7451e6dcb03d
Tomas Michael <tomasmichael995@gmail.com>
2018-11-06 12:57:43 +0200

    BluetoothUtils.getDeviceNamesList() accepts a Collection instead of a List

--- app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/utils/BluetoothDeviceUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/utils/BluetoothDeviceUtils.java
@@ -3,10 +3,11 @@ package gr.kalymnos.sk3m3l10.mybluetoothchat.utils;
 import android.bluetooth.BluetoothDevice;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class BluetoothDeviceUtils {
-    public static List<String> getDeviceNamesList(List<BluetoothDevice> devices) {
+    public static List<String> getDeviceNamesList(Collection<BluetoothDevice> devices) {
         List<String> list = new ArrayList<>();
         for (BluetoothDevice device : devices) {
             list.add(device.getName());
