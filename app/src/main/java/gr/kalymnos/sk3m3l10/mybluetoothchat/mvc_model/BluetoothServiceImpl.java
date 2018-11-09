524a9272c4f6cb9d4d3af3e15162816aa8a445e6
Tomas Michael <tomasmichael995@gmail.com>
2018-11-09 11:43:16 +0200

    Implemented manageClientsConnectedSocket() in BluetoothServiceImpl

--- app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_model/BluetoothServiceImpl.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/mybluetoothchat/mvc_model/BluetoothServiceImpl.java
@@ -2,6 +2,14 @@ package gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_model;
 
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
+
+import static gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_model.BluetoothConstants.Actions.ACTION_CLIENT_CONNECTED;
+import static gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_model.BluetoothConstants.Extras.EXTRA_DEVICE_NAME;
+import static gr.kalymnos.sk3m3l10.mybluetoothchat.mvc_model.BluetoothConstants.Extras.EXTRA_SOCKET_WRAPPER;
 
 public class BluetoothServiceImpl extends BluetoothService {
 
@@ -25,6 +33,16 @@ public class BluetoothServiceImpl extends BluetoothService {
 
     @Override
     protected void manageClientsConnectedSocket(String deviceName, BluetoothSocket bluetoothSocket) {
+        Intent clientData = new Intent(ACTION_CLIENT_CONNECTED);
+        clientData.putExtras(getBundledClientData(deviceName, bluetoothSocket));
+        LocalBroadcastManager.getInstance(getContext()).sendBroadcast(clientData);
+    }
 
+    @NonNull
+    private Bundle getBundledClientData(String deviceName, BluetoothSocket bluetoothSocket) {
+        Bundle extras = new Bundle();
+        extras.putString(EXTRA_DEVICE_NAME, deviceName);
+        extras.putParcelable(EXTRA_SOCKET_WRAPPER, new ParcelableBluetoothSocketWrapper(bluetoothSocket));
+        return extras;
     }
 }
